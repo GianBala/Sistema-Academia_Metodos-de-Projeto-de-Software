a209efafdb88749eff82ee51005841bee7c5334b
GianBala <giancarlo13pb@gmail.com>
2025-12-10 23:21:27 -0300

    classe main

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from user_interface.Menu_Principal import Menu_Principal
+class Main:
+    def __init__(self):
+        pass
 
-Menu_Principal.menu_principal()
+    def run():
+        Menu_Principal.menu_principal()
