7ad0c98e0350a93b85f0a2f01f1a0da7152f616c
Deivison <costadeivison007@gmail.com>
2026-02-10 10:29:31 -0300

    refactor: Remove codigo Python e limpa .gitignore para projeto Java
    Remove todos os arquivos Python (entidades, gerenciadores, menus, utils,
    main.py, requirements.txt) e o diretorio data.
    Simplifica .gitignore mantendo apenas regras relevantes para Java/Maven.

--- main.py
+++
@@ -1,9 +0,0 @@
-from src.user_interface.Menu_Principal import MenuPrincipal
-import src.utils.BancoDeDados.sqlite as sql
-
-# Conexão com banco de Dados
-db = sql.BancoDeDados()
-db.ChecarseBancoExiste()
-
-# Chama parte principal da Aplicação
-MenuPrincipal.menu_principal()
--- src/use_cases/Entidades/Aluno.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from src.use_cases.Entidades.Entidade import Entidade
-
-class Aluno(Entidade):
-
-    def __init__(self, nome: str, dt_nascimento: str, email: str, matricula: Optional[int] = None):
-        super().__init__(nome, dt_nascimento, email)
-        self.matricula = matricula
-        
